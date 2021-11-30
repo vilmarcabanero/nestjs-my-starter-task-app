9dd02435a78bcad807b793aa42e605f2fc07e8e7
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-11-30 12:19:51 +0800

    Fix cors

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -4,21 +4,21 @@ import { GetUser } from 'src/decorators/get.user.decorator';
 import { AuthService } from '.';
 import { RegisterPayload } from './register.payload';
 
-@Controller('api/auth')
+@Controller('/api/auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('register')
+  @Post('/register')
   async register(@Body() payload: RegisterPayload): Promise<object> {
     return this.authService.register(payload);
   }
 
-  @Post('login')
+  @Post('/login')
   async login(@Body() payload: RegisterPayload): Promise<object> {
     return this.authService.login(payload);
   }
 
-  @Get('user')
+  @Get('/user')
   @UseGuards(AuthGuard())
   async getUser(@GetUser() user: any): Promise<any> {
     return this.authService.getUser(user._id);
